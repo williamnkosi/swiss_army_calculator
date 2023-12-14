d155ce12c9c4d124bd2fe5c7bc29e67cd267ec96
William Nkosi <william.dev.acc@gmail.com>
2023-12-13 20:57:19 -0500

    Refactored code

--- lib/main.dart
+++ lib/main.dart
@@ -12,61 +12,33 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => BottomNavigationBarCubit(),
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
+      home: BlocProvider(
+        create: (context) => BottomNavigationBarCubit(),
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: const Text('Swiss Army Calculator'),
+          ),
+          body: BlocBuilder<BottomNavigationBarCubit, BottomNavigationBarState>(
+            builder: (context, state) {
+              if (state.type.index == 0) {
+                return const Center(child: HomePage());
+              } else if (state.type.index == 1) {
+                return const Center(child: FavoritesPage());
+              } else {
+                return const Center(child: AccountPage());
+              }
+            },
+          ),
+          bottomNavigationBar: const AppBottomNavigationBar(),
         ),
-        body: BlocBuilder<BottomNavigationBarCubit, BottomNavigationBarState>(
-          builder: (context, state) {
-            if (state.type.index == 0) {
-              return const Center(child: HomePage());
-            } else if (state.type.index == 1) {
-              return const Center(child: FavoritesPage());
-            } else {
-              return const Center(child: AccountPage());
-            }
-          },
-        ),
-        bottomNavigationBar: const AppBottomNavigationBar(),
-        // This trailing comma makes auto-formatting nicer for build methods.
       ),
     );
   }
